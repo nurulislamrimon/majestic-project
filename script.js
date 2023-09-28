d6d8eca2f5cd173ddea22bcc40612fc68c2f6610
nurulislamrimon <nurulislamrimon@gmail.com>
2023-09-28 21:46:12 +0600

    country selection core ready

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
+// =======================================
+// ======country selection section========
+// =======================================
 const searchBox = document.getElementById("search-country-input");
 const countriesContainer = document.getElementById("countries-container");
 
 searchBox.addEventListener("keyup", function (event) {
   const searchKey = event.target.value;
+  //   const fetchTimeout = setTimeout(() => {
   getFilteredData(searchKey);
+  //     clearTimeout(fetchTimeout);
+  //   }, 500);
 });
 
 const getFilteredData = (searchKey) => {
   if (!searchKey) {
     searchKey = "a";
   }
-  fetch(`https://restcountries.com/v3.1/name/${searchKey}?fields=name,flags`)
+  fetch(
+    `https://restcountries.com/v3.1/name/${searchKey}?sort=desc&fields=name,flags`
+  )
     .then((response) => response.json())
     .then((data) => {
       renderCountries(data);
@@ -22,7 +30,12 @@ const renderCountries = (countries) => {
   if (countries.length) {
     countries?.map((country) => {
       const newDiv = document.createElement("div");
-      newDiv.classList.add("d-flex", "align-items-center");
+      newDiv.classList.add(
+        "d-flex",
+        "align-items-center",
+        "px-3",
+        "payment-modal-country"
+      );
       newDiv.innerHTML = `<img src=${country?.flags?.svg} alt="" height='18' width='24'/>
     <p style="font-size: 18px; margin-top: 12px; margin-left: 20px">${country?.name?.common}</p>`;
       countriesContainer.appendChild(newDiv);
@@ -32,3 +45,7 @@ const renderCountries = (countries) => {
   }
 };
 getFilteredData("a");
+
+// =======================================
+// ======2 selection section========
+// =======================================
